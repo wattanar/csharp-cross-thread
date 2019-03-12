1a6cee509b412517c23f56c77d5794b8acd94388
Wattana Ruengmucha <iamwattanas@gmail.com>
2019-03-12 11:27:01 +0700

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public delegate void UpdateControlsDelegate(int i);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread background = new Thread(Delay);
+            background.IsBackground = true;
+            background.Start();
+        }
+
+        private void Delay()  
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                if (label1.InvokeRequired)
+                {
+                    this.Invoke(new UpdateControlsDelegate(UpdateControls), new Object[] { i });
+                }
+                else
+                {
+                    UpdateControls(i);
+                }
+                
+            }
+        }
+
+        private void UpdateControls(int i)
+        {
+            label1.Text = " Running..." + i;
+        }
+    }
+}
